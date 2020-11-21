53681a2ecbf18e35bdfa6f2892a1429c9b8d598e
DigitalCitizen110625 <bkarner5307@conestogac.on.ca>
2020-11-21 10:15:45 -0500

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # QualityFootCareSolutions-Public
 Public repository for the QFCS website
+
+## :cloud:	Live Demo
+<a href="https://www.qualityfootcaresolutions.ca/">Click here</a> for a working demo of the website
+
+
+## :memo:	About
+Quality foot care solutions is a small business website for providing local foot care services. 
--- php/Email.config.php
+++ php/Email.config.php
@@ -1,12 +1,12 @@
 <?php
 
 //Define the SMTP settings for the AWS Simple Email Service
-define("EMAIL_TO_ADDRESS", "<EMAIL_TO_ADDRESS>");
-define("EMAIL_FROM_ADDRESS", "<EMAIL_FROM_ADDRESS>");
-define("EMAIL_FROM_NAME", "<EMAIL_FROM_NAME>");
-define("EMAIL_SMTP_USERNAMAE", "<EMAIL_SMTP_USERNAMAE>");
-define("EMAIL_SMTP_PASSWORD", "<EMAIL_SMTP_PASSWORD>");
-define("EMAIL_HOST", "<EMAIL_HOST>");
-define("EMAIL_PORT", "<EMAIL_PORT>");
-define("EMAIL_BODY_TEMPLATE", '<EMAIL_BODY_TEMPLATE>');
+define("EMAIL_TO_ADDRESS", "<REMOVED>");
+define("EMAIL_FROM_ADDRESS", "<REMOVED>");
+define("EMAIL_FROM_NAME", "<REMOVED>");
+define("EMAIL_SMTP_USERNAMAE", "<REMOVED>");
+define("EMAIL_SMTP_PASSWORD", "<REMOVED>");
+define("EMAIL_HOST", "<REMOVED>");
+define("EMAIL_PORT", "<REMOVED>");
+define("EMAIL_BODY_TEMPLATE", '<REMOVED>');
 ?>
